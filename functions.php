a083d483ffd334edd7218ac3c8f771b98d941d3d
Atul <atul.kumar@wisdmlabs.com>
2023-04-24 19:14:32 +0530

    completed with the code, the primary tag is on its full width

--- functions.php
+++ functions.php
@@ -28,10 +28,21 @@ if ( ! function_exists( 'twenty_twenty_one_setup' ) ) {
     add_action( 'after_setup_theme', 'twenty_twenty_one_setup' );
 }
 
-if ( ! function_exists( 'wp_get_list_item_separator' ) ) :
+if(! function_exists( 'register_style' )){
 
-	function wp_get_list_item_separator() {
-		
-		return __( ', ', 'Primary-template' );
-	}
-endif;
+    function register_style(){
+
+        wp_enqueue_style('style', get_stylesheet_uri());
+        
+        //for full_width
+        if(is_page_template('max_width.php'))
+        {
+            wp_enqueue_style('max_width_styles',get_template_directory_uri().'/max_width.css',false,1.0,'all');
+        }
+    
+   
+    
+    }
+    add_action('wp_enqueue_scripts','register_style');
+
+}
